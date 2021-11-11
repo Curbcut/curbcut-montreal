015fda80580c1830b12568728ba204d7062cbabc
Maxime Bélanger De Blois <maximebdeblois@gmail.com>
2021-11-11 17:42:04 -0500

    Big updates on green alleys

--- R/global_alley.R
+++ R/global_alley.R
@@ -31,3 +31,26 @@ var_list_alley <-
          "15 minutes to work (%)" = "trans_t_15_prop",
          "15-45 minutes to work (%)" = "trans_t_45_prop",
          "More than 45 minutes to work (%)" = "trans_t_45_plus_prop"))
+
+
+# Fill color --------------------------------------------------------------
+
+alleys <- 
+alleys %>% 
+  mutate(fill = case_when(type == "green" ~ "#008100EE",
+                         type == "community" ~ "#F6BE00EE",
+                         type == "mixed" ~ "#B37400EE",
+                         type == "none" ~ "#262626EE",
+                         TRUE ~ NA_character_))
+
+# Legend ------------------------------------------------------------------
+
+alley_legend_en <- 
+  mapdeck_legend(
+    legend_element(
+      variables = c("Green", "Community", "Mixed", "None"),
+      colours = c("#008100EE","#F6BE00EE", "#B37400EE", "#262626EE"),
+      colour_type = "fill",
+      variable_type = "category",
+      title = "Green alley type")
+  )
