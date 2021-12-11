23b40a9aa3f3a0b8648bafad739cd5cfad1e4ae8
Maxime Bélanger De Blois <maximebdeblois@gmail.com>
2021-12-11 11:24:07 -0500

    Full list of housing variables, and many updates in get_census_vectors

--- dev/modules/census/census_functions_new.R
+++ dev/modules/census/census_functions_new.R
@@ -21,23 +21,91 @@ get_empty_geometries <- function(scales, years) {
 
 # Download variables ------------------------------------------------------
 
-get_census_vectors <- function(census_vec, geoms, scales, years) {
+get_census_vectors <- function(census_vec, geoms, scales, years, parent_vectors = NULL) {
   map2(scales, geoms, function(scale, g) {
     map2(years, g, function(year, df_g) {
+      
+      census_dataset <- paste0("CA", sub("20", "", year))
+      
+      original_vectors_named <- set_names(pull(census_vec, all_of(paste0("vec_", year))), 
+                                          census_vec$var_code)
+      
+      original_vectors_named <- original_vectors_named[!is.na(original_vectors_named)]
+      
+      # Get original vectors
+      original_vectors_retrieved <- 
       cancensus::get_census(
-        dataset = paste0("CA", sub("20", "", year)),
+        dataset = census_dataset,
         regions = list(CMA = "24462"),
         level = scale,
-        vectors = set_names(pull(census_vec, all_of(paste0("vec_", year))), 
-                            census_vec$var_code),
+        vectors = original_vectors_named,
         geo_format = NA,
         quiet = TRUE) |> 
-        select(GeoUID, all_of(census_vec$var_code)) |> 
-        right_join(df_g, by = "GeoUID") |> 
-        st_as_sf() |> 
-        st_set_agr("constant")
+        select(GeoUID, any_of(census_vec$var_code))
+      
+      # Parent vectors must be "mapped", as they might not be unique census vectors.
+      # Some vectors share the same denominators, yet we want them all to have their
+      # _parent suffix.
+      parent_vecs <- 
+        cancensus::list_census_vectors(census_dataset) %>% 
+        filter(vector %in% original_vectors_named) %>% 
+        arrange(match(vector, original_vectors_named)) %>% 
+        # In cases of pct, there is no parent_vector. Yet, they tell what's the
+        # vector that will be used to weight the averages in the aggregation column.
+        mutate(parent_vector = ifelse(is.na(parent_vector), str_extract(aggregation, "v_.*$"),
+                                      parent_vector)) %>% 
+        pull(parent_vector) %>% 
+        set_names(paste0(names(original_vectors_named), "_parent"))
+      
+      # Retrieve the values of all parent vectors
+      parent_vector_values <- map(names(parent_vecs), ~{
+        vec <- set_names(parent_vecs[.x], .x)
+        retrieved_parent <- 
+        cancensus::get_census(
+          dataset = census_dataset,
+          regions = list(CMA = "24462"),
+          level = scale,
+          vectors = vec[!is.na(vec)],
+          geo_format = NA,
+          quiet = TRUE) %>% 
+          select(GeoUID, any_of(.x))
+        
+        # In cases of errors in cancensus, we can use the parent_vectors argument to
+        # add parent vectors.
+        if (.x %in% paste0(names(parent_vectors), "_parent")) {
+          names(parent_vectors) <- paste0(names(parent_vectors), "_parent")
+          vec <-  parent_vectors[names(parent_vectors) == .x[.x == names(parent_vectors)]]
+
+          retrieved_parent <-
+            cancensus::get_census(
+              dataset = census_dataset,
+              regions = list(CMA = "24462"),
+              level = scale,
+              vectors = vec[!is.na(vec)],
+              geo_format = NA,
+              quiet = TRUE) %>%
+            select(GeoUID, any_of(.x))
+        }
+          
+        # If there's missing parent vectors, it gives an error and tells us where
+        if(ncol(retrieved_parent) != 2) {
+          stop(paste0(year, ", ", scale, ", no parent vector for ", .x))
+        }
+        
+          retrieved_parent
       })
+
+      # Join the dfs
+      left_join(original_vectors_retrieved,
+                reduce(parent_vector_values, left_join, by = "GeoUID"),
+                by = "GeoUID") |>
+        right_join(df_g, by = "GeoUID") |>
+        st_as_sf() |>
+        st_set_agr("constant")
+      
+    })
   })
+  
 }
 
 
--- dev/modules/census/census_housing.R
+++ dev/modules/census/census_housing.R
@@ -14,8 +14,6 @@ census_housing <- tibble(
   vec_2001 = character(),
   vec_1996 = character(),
   include = logical(),
-  interpolation = character(),
-  denominator = character(),
   var_title = character(),
   var_short = character(),
   explanation = character(),
@@ -26,15 +24,6 @@ census_housing <- tibble(
 census_housing <- 
   census_housing |> 
   add_row(
-    var_code = "tenure_households",
-    vec_2016 = "v_CA16_4836",
-    vec_2011 = "v_CA11N_2252",
-    vec_2006 = "v_CA06_101",
-    vec_2001 = "v_CA01_96",
-    vec_1996 = "v_CA1996_1678",
-    include = FALSE,
-    interpolation = "count") |> 
-  add_row(
     var_code = "housing_tenant_pct",
     vec_2016 = "v_CA16_4838",
     vec_2011 = "v_CA11N_2254",
@@ -42,14 +31,107 @@ census_housing <-
     vec_2001 = "v_CA01_100",
     vec_1996 = "v_CA1996_1683",
     include = TRUE,
-    interpolation = "count",
-    denominator = "tenure_households",
     var_title = "Tenant-occupied (%)",
     var_short = "Tenant (%)",
     explanation = "the percentage of private dwellings occupied by tenants",
+    private = FALSE) |> 
+  add_row(
+    var_code = "housing_rent_avg_dollar",
+    vec_2016 = "v_CA16_4901",
+    vec_2011 = "v_CA11N_2292",
+    vec_2006 = "v_CA06_2050",
+    vec_2001 = "v_CA01_1667",
+    vec_1996 = "v_CA1996_1701",
+    include = TRUE,
+    var_title = "Average rent ($)",
+    var_short = "TKTK",
+    explanation = "the average rent paid by tenants per month",
+    private = FALSE) |>
+  add_row(
+    var_code = "housing_repairs_pct",
+    vec_2016 = "v_CA16_4872",
+    vec_2011 = "v_CA11N_2232",
+    vec_2006 = "v_CA06_108",
+    vec_2001 = "v_CA01_104",
+    vec_1996 = "v_CA1996_1687",
+    include = TRUE,
+    var_title = "Housing requiring major repairs (%)",
+    var_short = "TKTK",
+    explanation = "the percentage of households living in dwellings requiring major repairs",
+    private = FALSE) |>
+  add_row(
+    var_code = "housing_value_avg_dollar",
+    vec_2016 = "v_CA16_4896",
+    vec_2011 = "v_CA11N_2287",
+    vec_2006 = "v_CA06_2054",
+    vec_2001 = "v_CA01_1674",
+    vec_1996 = "v_CA1996_1681",
+    include = TRUE,
+    var_title = "Average property value ($)",
+    var_short = "TKTK",
+    explanation = "the average value of owner-occupied dwellings",
+    private = FALSE) |>
+  add_row(
+    var_code = "housing_unafford_pct",
+    vec_2016 = "v_CA16_4888",
+    include = TRUE,
+    var_title = "Unaffordable housing (%)",
+    var_short = "TKTK",
+    explanation = "the percentage of dwellings for which residents pay more than 30% of income on housing costs",
+    private = FALSE) |>
+  add_row(
+    var_code = "housing_unsuit_pct",
+    vec_2016 = "v_CA16_4861",
+    vec_2011 = "v_CA11N_2276",
+    include = TRUE,
+    var_title = "Unsuitable housing (%)",
+    var_short = "TKTK",
+    explanation = "the percentage of households living in accommodations without enough bedrooms according to the National Occupancy Standard",
+    private = FALSE) |>
+  add_row(
+    var_code = "housing_stress_renter_pct",
+    vec_2016 = "v_CA16_4899",
+    vec_2011 = "v_CA11N_2290",
+    include = TRUE,
+    var_title = "Renter housing stress (%)",
+    var_short = "TKTK",
+    explanation = "the percentage of renter households that spend more than 30% of their income on shelter costs",
+    private = FALSE) |>
+  add_row(
+    var_code = "housing_stress_owner_pct",
+    vec_2016 = "v_CA16_4892",
+    vec_2011 = "v_CA11N_2283",
+    include = TRUE,
+    var_title = "Owner housing stress (%)",
+    var_short = "TKTK",
+    explanation = "the percentage of owner households that spend more than 30% of their income on shelter costs",
+    private = FALSE) |>
+  add_row(
+    var_code = "housing_mobility_one_pct",
+    vec_2016 = "v_CA16_6698",
+    vec_2011 = "v_CA11N_1723",
+    vec_2006 = "v_CA06_453",
+    vec_2001 = "v_CA01_383",
+    vec_1996 = "v_CA1996_1387",
+    include = TRUE,
+    var_title = "One-year housing mobility (%)",
+    var_short = "TKTK",
+    explanation = "the percentage of households that have moved in the past year",
+    private = FALSE) |>
+  add_row(
+    var_code = "housing_mobility_five_pct",
+    vec_2016 = "v_CA16_6725",
+    vec_2011 = "v_CA11N_1750",
+    vec_2006 = "v_CA06_462",
+    vec_2001 = "v_CA01_392",
+    vec_1996 = "v_CA1996_1396",
+    include = TRUE,
+    var_title = "Five-year housing mobility (%)",
+    var_short = "TKTK",
+    explanation = "the percentage of households that have moved in the past five years",
     private = FALSE)
   
-
+  
 # Get empty geometries ----------------------------------------------------
 
 geoms <- get_empty_geometries(scales, years)
@@ -57,19 +139,20 @@ geoms <- get_empty_geometries(scales, years)
 
 # Download data -----------------------------------------------------------
 
-data_raw <- get_census_vectors(census_housing, geoms, scales, years)
+data_raw <- get_census_vectors(census_housing, geoms, scales, years,
+                               parent_vectors = c("housing_value_avg_dollar" = "v_CA01_1670"))
 
 
 # Interpolate -------------------------------------------------------------
 
-var_count <- 
-  census_housing |> 
-  filter(interpolation == "count") |> 
-  pull(var_code)
-
 var_avg <- 
   census_housing |> 
-  filter(interpolation == "average") |> 
+  filter(str_detect(var_code, "pct|avg")) |> 
+  pull(var_code)
+
+var_count <- 
+  census_housing |> 
+  filter(!var_code %in% var_avg) |> 
   pull(var_code)
 
 data_inter <- interpolate(data_raw, scales, years)
