771c118148e951f9d8b9dfc7a115cbe452eac742
Maxime Bélanger De Blois <maximebdeblois@gmail.com>
2021-12-16 14:18:14 +0100

    census income ready to go

--- dev/modules/census/census_income.R
+++ dev/modules/census/census_income.R
@@ -23,16 +23,16 @@ census_income <- tibble(
 add_row_inc <- function(data, var_code, vec_2016, vec_2011, vec_2006, vec_2001,
                         vec_1996, var_title, var_short, explanation, private) {
   add_row(data,
-    var_code = var_code,
-    vec_2016 = list(vec_2016),
-    vec_2011 = list(vec_2011),
-    vec_2006 = list(vec_2006),
-    vec_2001 = list(vec_2001),
-    vec_1996 = list(vec_1996),
-    var_title = var_title,
-    var_short = var_short,
-    explanation = explanation,
-    private = private
+          var_code = var_code,
+          vec_2016 = list(vec_2016),
+          vec_2011 = list(vec_2011),
+          vec_2006 = list(vec_2006),
+          vec_2001 = list(vec_2001),
+          vec_1996 = list(vec_1996),
+          var_title = var_title,
+          var_short = var_short,
+          explanation = explanation,
+          private = private
   )
 }
 
@@ -65,44 +65,37 @@ census_income <-
     explanation = "the percentage of households with an income less then $50,000",
     private = FALSE
   ) |>
-  # add_row(
-  #   var_code = "inc_100_pct",
-  #   vec_2016 = "v_CA16_4901",
-  #   vec_2011 = "v_CA11N_2292",
-  #   vec_2006 = "v_CA06_2050",
-  #   vec_2001 = "v_CA01_1667",
-  #   vec_1996 = "v_CA1996_1701",
-  #   include = TRUE,
-  #   var_title = "Income beetween $50k-$100k (%)",
-  #   var_short = "TKTK",
-  #   explanation = "the percentage of households with an income between $50,000 and $100,000",
-  #   private = FALSE) |>
+  add_row_inc(
+    var_code = "inc_100_pct",
+    vec_2016 = paste0("v_CA16_24", 16:20),
+    vec_2011 = paste0("v_CA11N_25", 41:43),
+    vec_2006 = paste0("v_CA06_19", 94:98),
+    vec_2001 = paste0("v_CA01_16", 27:31),
+    vec_1996 = paste0("v_CA1996_16", 20:24),
+    include = TRUE,
+    var_title = "Income beetween $50k-$100k (%)",
+    var_short = "TKTK",
+    explanation = "the percentage of households with an income between $50,000 and $100,000",
+    private = FALSE) |>
   add_row_inc(
     var_code = "inc_high_pct",
-    vec_2016 = "v_CA16_4872",
-    vec_2011 = "v_CA11N_2232",
-    vec_2006 = "v_CA06_108",
-    vec_2001 = "v_CA01_104",
-    vec_1996 = "v_CA1996_1687",
+    vec_2016 = "v_CA16_2421",
+    vec_2011 = "v_CA11N_2546",
+    vec_2006 = "v_CA06_1999",
+    vec_2001 = "v_CA01_1632",
+    vec_1996 = "v_CA1996_1625",
     var_title = "Income above $100k (%)",
     var_short = "TKTK",
     explanation = "the percentage of households with an income higher than $100,000",
     private = FALSE
-  )
-
-# |>
-# add_row_inc(
-#   var_code = "inc_limat_prop",
-#   vec_2016 = "v_CA16_4896",
-#   vec_2011 = "v_CA11N_2287",
-#   vec_2006 = "v_CA06_2054",
-#   vec_2001 = "v_CA01_1674",
-#   vec_1996 = "v_CA1996_1681",
-#   include = TRUE,
-#   var_title = "Prevalence of low income (after-tax) (%)",
-#   var_short = "TKTK",
-#   explanation = "the prevalence of low income in private households based on the Low income measure, after-tax(LIM-AT)",
-#   private = FALSE)
+  ) |>
+  add_row_inc(
+    var_code = "inc_limat_prop",
+    vec_2016 = "v_CA16_2540",
+    var_title = "Prevalence of low income (after-tax) (%)",
+    var_short = "TKTK",
+    explanation = "the prevalence of low income in private households based on the Low income measure, after-tax(LIM-AT)",
+    private = FALSE)
 
 
 # Gather data -------------------------------------------------------------
