a06e8ba8fbee557e15dd0af379ac11c5a8f26cdf
David Wachsmuth <david.wachsmuth@mcgill.ca>
2021-12-16 10:46:59 -0500

    Bug fixes

--- dev/modules/census/census_functions_new.R
+++ dev/modules/census/census_functions_new.R
@@ -160,7 +160,7 @@ get_census_vectors <- function(census_vec, geoms, scales, years,
       })
       
       # Join the dfs
-      orig_vec_retrieved |> 
+      vec_retrieved |> 
         left_join(reduce(parent_vec_values, left_join, by = "GeoUID"),
                   by = "GeoUID") |>
         right_join(df, by = "GeoUID") |>
@@ -842,40 +842,39 @@ add_census_data <- function(census_vec, scales, years, parent_vectors = NULL,
                             CMA = "24462", crs = 32618) {
   
   # Get empty geometries
-  message("Getting empty geometries ...")
-  geoms <- get_empty_geometries(scales, years, CMA, CRS)
+  message("Getting empty geometries...")
+  geoms <- get_empty_geometries(scales, years, CMA, crs)
   
   # Download data
-  message("Downloading census data ...")
+  message("Downloading census data...")
   data_raw <- get_census_vectors(census_vec, geoms, scales, years,
                                  parent_vectors, CMA)
   
   # Get aggregation type
-  message("Interpolating ...", appendLF = TRUE)
-  data_aggregation <- get_aggregation_type(census_vec, scales, years)
-  ## Interpolate
-  data_inter <- interpolate(data_raw, scales, years, data_aggregation, census_vec)
+  message("Interpolating...")
+  data_agg <- get_agg_type(census_vec, scales, years)
+  
+  # Interpolate
+  data_inter <- interpolate(data_raw, scales, years, 
+                            data_aggregation = data_agg,
+                            census_vec = census_vec)
   ## Swap CSD to borough
-  message("Swapping CSD to borough ...", appendLF = TRUE)
+  message("Swapping CSD to borough...")
   data_swaped <- swap_csd_to_borough(data_inter, years, var_count, var_avg)
   # From here, no CSD, but borough
   scales[scales == "CSD"] <- "borough"
   ## Interpolate to building, grid & street
-  message("Interpolating other geometries (grid, ... ) ...",
-    appendLF = TRUE
-  )
-  data_other_inter <- interpolate_other_geoms(
-    c("grid"),
-    data_swaped, years,
-    var_count, var_avg
-  )
+  message("Interpolating other geometries (grid, ... ) ...")
+  data_other_inter <- interpolate_other_geoms(c("grid"),
+                                              data_swaped, years,
+                                              var_count, var_avg)
   ## Get units type
-  message("Normalizing all data ...", appendLF = TRUE)
+  message("Normalizing all data ...")
   data_unit <- get_unit_type(census_vec, scales, years)
   ## Normalize pct variables
   data_norm <- normalize(data_other_inter, census_vec, data_unit)
   ## Drop variables which aren't included in final tables
-  message("Other manipulations (dropping variables, q3, q5, ...) ...", appendLF = TRUE)
+  message("Other manipulations (dropping variables, q3, q5, ...) ...")
   data_final <- drop_vars(data_norm, census_vec)
   ## Add q3 and q5 versions
   cat_q5 <- get_categories_q5(data_final, census_vec)
@@ -887,6 +886,6 @@ add_census_data <- function(census_vec, scales, years, parent_vectors = NULL,
   ## Add years
   data_years <- add_years(data_breaks, years)
   ## Finalize output
-  message("Reducing ...", appendLF = TRUE)
+  message("Reducing ...")
   reduce_years(data_years)
 }
