35e7adead3e9631a0c36bf75b1dfb3940edc7ba7
David Wachsmuth <dwachsmuth@MBP-2021.local>
2022-01-06 22:02:51 -0500

    Update to dl_unzip function

--- dev/build_from_db.R
+++ dev/build_from_db.R
@@ -1,49 +1,35 @@
-# Update data folders from a new run of build_data ##############################
-
+# Update data folders from a new run of build_data #############################
 
 # Download and unzip updated data -----------------------------------------
 
 dl_unzip <- function(dir, data_url) {
   
-  # Set working directory for decompression
-  # simplifies unzip directory location behavior
-  wd <- getwd()
-  setwd(dir)
-  
-  unlink(list.files(full.names = TRUE))
-  
-  
-  # Download zip file
-  download.file(data_url,
-                destfile = "temp.zip",
-                mode = "wb")
-  
-  # Run decompression
-    system2("unzip",
-            args = c("-o", # include override flag
-                     "temp.zip"),
-            stdout = TRUE)
-  
-  # uncomment to delete archive once decompressed
-  file.remove("temp.zip")
-  
-  # Reset working directory
-  setwd(wd); rm(wd)
-  
-  
+  if (Sys.info()["sysname"] != "Windows") {
+    withr::with_options(list(timeout = 2000), {
+      download.file(data_url, destfile = "temp.zip")
+      unzip("temp.zip", unzip = "unzip", exdir = dir)
+      file.remove("temp.zip")
+      return(TRUE)
+    })
+  } else {
+    wd <- getwd()
+    setwd(dir)
+    unlink(list.files(full.names = TRUE))
+    download.file(data_url, destfile = "temp.zip", mode = "wb")
+    system2("unzip", args = c("-o", "temp.zip"), stdout = TRUE)
+    file.remove("temp.zip")
+    setwd(wd)
+  }
 }
 
 
 # Run the function on all data folders ------------------------------------
 
 # dev_data
-dl_unzip("dev/data",
-         .sus_links[1])
+dl_unzip("dev/data", .sus_links[1])
 
 # data
-dl_unzip("data",
-         .sus_links[2])
+dl_unzip("data", .sus_links[2])
 
 # www_maps
-dl_unzip("www/maps",
-         .sus_links[3])
+dl_unzip("www/maps", .sus_links[3])
