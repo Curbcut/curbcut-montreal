0bafc13e9c0feb146ad79247ec6020776f1bc1bd
David Wachsmuth <dwachsmuth@MBP-2021.local>
2022-01-10 12:47:41 -0500

    Fix to simplification bug

--- dev/other/simplify.R
+++ dev/other/simplify.R
@@ -2,6 +2,21 @@
 
 suppressPackageStartupMessages(library(rmapshaper))
 
-borough$geometry <- ms_simplify(borough$geometry, keep = 0.5, keep_shapes = TRUE)
-CT$geometry <- ms_simplify(CT$geometry, keep = 0.5, keep_shapes = TRUE)
-DA$geometry <- ms_simplify(DA$geometry, keep = 0.5, keep_shapes = TRUE)
+borough$geometry <- 
+  borough$geometry |> 
+  st_transform(32618) |> 
+  ms_simplify(keep = 0.5, keep_shapes = TRUE) |> 
+  st_transform(4326)
+
+CT$geometry <-
+  CT$geometry |> 
+  st_transform(32618) |> 
+  ms_simplify(keep = 0.5, keep_shapes = TRUE) |> 
+  st_transform(4326)
+
+DA$geometry <-
+  DA$geometry |> 
+  st_transform(32618) |> 
+  ms_simplify(keep = 0.5, keep_shapes = TRUE) |> 
+  st_transform(4326)
+
