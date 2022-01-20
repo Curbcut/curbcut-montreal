e6ad24d095d6d01498c8dfc1e0ecf32f14a04476
dwachsmuth <david.wachsmuth@mcgill.ca>
2022-01-19 22:59:30 -0500

    Explore show/hide clean up

--- R/m_explore.R
+++ R/m_explore.R
@@ -16,23 +16,17 @@ explore_UI <- function(id) {
   
   tagList(
     
-    conditionalPanel(
-      condition = "output.show_panel == true", ns = NS(id),
-      fluidRow(column(width = 7, h4(i18n$t("Explore"))),
-               column(width = 5, align = "right", 
-                      actionLink(inputId = NS(id, "hide"), 
-                                 label = i18n$t("Hide"))))),
+    div(id = NS(id, "explore_title"),
+        fluidRow(column(width = 7, h4(i18n$t("Explore"))),
+                 column(width = 5, align = "right", 
+                        actionLink(inputId = NS(id, "hide"), 
+                                   label = i18n$t("Hide"))))),
     
-    conditionalPanel(
-      condition = "output.hide_status == 1", ns = NS(id),
-      htmlOutput(NS(id, "info_table")),
-      conditionalPanel(
-        condition = "output.show_graph == true", ns = NS(id),
-        plotOutput(NS(id, "explore_graph"), height = 150)),
-      conditionalPanel(
-        condition = "output.poly_selected == 1", ns = NS(id),
-        actionLink(inputId = NS(id, "clear_selection"),
-                   label = "Clear selection")))
+    div(id = NS(id, "explore_content"),
+        htmlOutput(NS(id, "info_table")),
+        plotOutput(NS(id, "explore_graph"), height = 150),
+        hidden(actionLink(inputId = NS(id, "clear_selection"),
+                          label = "Clear selection")))
   )
 }
 
@@ -68,9 +62,10 @@ explore_server <- function(id, data, var_left, var_right, df, select_id,
         build_str_as_DA = build_str_as_DA)
       })
     
-    # Display info_table if it isn't NULL
-    output$info_table <- renderUI(if (!is.null(table())) table())
+    # Display info table
+    output$info_table <- renderUI(table())
     
+    # Make graph
     graph <- reactive(explore_graph(
       data = data(),
       var_type = var_type(), 
@@ -81,33 +76,24 @@ explore_server <- function(id, data, var_left, var_right, df, select_id,
       build_str_as_DA = build_str_as_DA,
       plot_type = "auto"))
     
-    # Display graph if it isn't NULL
-    output$explore_graph <- renderPlot(if (!is.null(graph())) graph())
+    # Display graph
+    output$explore_graph <- renderPlot(graph())
     
-    # Only show panel if there's something to show
-    show_panel <- reactive(!is.null(table()) || !is.null(graph()))
-    output$show_panel <- show_panel
-    outputOptions(output, "show_panel", suspendWhenHidden = FALSE)
+    # Show/hide components
+    observe({
+      toggle("explore_content", condition = 
+               (!is.null(table()) || !is.null(graph())) && input$hide %% 2 == 0)
+      toggle("explore_graph", condition = !is.null(graph()))
+      toggle("clear_selection", condition = !is.na(select_id()))
+    })
     
-    # Only show graph if there's something to show
-    show_graph <- reactive(!is.null(graph()))
-    output$show_graph <- show_graph
-    outputOptions(output, "show_graph", suspendWhenHidden = FALSE)
-    
-    # Hide explore status
-    output$hide_status <- reactive(show_panel() && input$hide %% 2 == 0)
-    outputOptions(output, "hide_status", suspendWhenHidden = FALSE)
-    
+    # Change show/hide button text
     observeEvent(input$hide, {
       if (input$hide %% 2 == 0) {
         txt <- sus_translate("Hide")
       } else txt <- sus_translate("Show")
       updateActionButton(session, "hide", label = txt)
     })
-    
-    # Hook up "Clear selection" button
-    output$poly_selected <- reactive({if (!is.na(select_id())) TRUE else FALSE})
-    outputOptions(output, "poly_selected", suspendWhenHidden = FALSE)
     
     # Return info_table text and graph to export it in report afterwards
     reactive({list(info = info_table(), graph = graph())})
