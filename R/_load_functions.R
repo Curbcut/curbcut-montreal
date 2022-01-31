78e7152282d5e6b5bde652f1e0d36d814014359a
Max <maximebdeblois@gmail.com>
2022-01-31 17:29:51 -0500

    New R subfolder, and script to load it as if it were directly in R/ folder.

---
+++ R/_load_functions.R
@@ -0,0 +1,19 @@
+### LOAD FUNCTIONS #############################################################
+
+#' This script is used to make the R folder a easier to navigate. The SUS custom
+#' functions are placed in the R/functions/ folder, and this script sources 
+#' them the same way Shiny would if they were directly in the R/ folder.
+
+
+# Load functions in R/functions/ ------------------------------------------
+
+# Hack here to get shiny environment, as when local is set to TRUE in a 
+# purrr::walk, it the source is executed inside the walk rather than in 
+# the "global" environment. We can get the current environment of the shiny
+# session, and feed it to the local argument.
+
+envir <- rlang::get_env()
+
+walk(list.files("R/functions/"), ~{
+  source(paste0("R/functions/", .x), local = envir)
+})
