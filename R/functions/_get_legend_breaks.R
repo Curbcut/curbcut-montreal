fcade12656ba0763cf975849ccd2c11b021fcdf8
dwachsmuth <david.wachsmuth@mcgill.ca>
2022-03-21 09:39:15 -0400

    Function clean up

--- R/functions/_get_legend_breaks.R
+++ R/functions/_get_legend_breaks.R
@@ -3,7 +3,7 @@
 get_legend_breaks <- function(data, var_left, var_right, df, data_type) {
   
   # Return NULL if no data_type matches
-  break_labels <- NULL
+  break_labs <- NULL
 
   
   ## Univariate q5 version -----------------------------------------------------
@@ -11,36 +11,30 @@ get_legend_breaks <- function(data, var_left, var_right, df, data_type) {
   if (data_type == "q5") {
     
     # Get break labels
-    break_labels <- 
-      variables |> 
-      filter(var_code == unique(sub("_\\d{4}$", "", var_left))) |> 
-      pull(breaks_q5) |> 
-      pluck(1) |> 
-      filter(scale == df)
+    break_labs <- variables$breaks_q5[
+      variables$var_code == unique(sub("_\\d{4}$", "", var_left))]
+    if (length(break_labs) > 0) break_labs <- 
+        break_labs[[1]][break_labs[[1]]$scale == df,]
     
-    if (suppressWarnings(!is.null(break_labels$var_name) && 
-                         !any(is.na(break_labels$var_name)))) {
+    if (suppressWarnings(!is.null(break_labs$var_name) && 
+                         !any(is.na(break_labs$var_name)))) {
       
       qual <- TRUE
-      
-      break_labels <- 
-        break_labels |> 
-        pull(var_name_short)
+      break_labs <- break_labs$var_name_short
       
     } else {
       
       qual <- FALSE
-      
-      break_labels <- break_labels$var
+      break_labs <- break_labs$var
       
       # Format break labels
       if (str_detect(var_left, "_pct|_dollar")) {
-        break_labels <- convert_unit(break_labels, var_left, TRUE)
+        break_labs <- convert_unit(break_labs, var_left, TRUE)
       }
     }
     
     # Add qual attribute
-    attr(break_labels, "qual") <- qual
+    attr(break_labs, "qual") <- qual
     
   }
   
@@ -60,112 +54,110 @@ get_legend_breaks <- function(data, var_left, var_right, df, data_type) {
     date_right <- str_extract(var_right, "(?<=_)\\d{4}$")
     
     # Get breaks
-    break_labels_y <- 
-      variables |> 
-      filter(var_code == unique(sub("_\\d{4}$", "", var_left))) |> 
-      pull(breaks_q3) |> 
-      pluck(1) |> 
-      filter(date == date_left | is.na(date)) |> 
-      filter(scale == df) |> 
-      pull(var)
+    break_labs_y <- variables$breaks_q3[
+      variables$var_code == unique(sub("_\\d{4}$", "", var_left))]
+    if (length(break_labs_y) > 0) break_labs_y <- 
+      break_labs_y[[1]]$var[
+        (break_labs_y[[1]]$date == date_left | 
+           is.na(break_labs_y[[1]]$date)) &
+          break_labs_y[[1]]$scale == df]
     
-    break_labels_x <- 
-      variables |> 
-      filter(var_code == unique(sub("_\\d{4}$", "", var_right))) |> 
-      pull(breaks_q3) |> 
-      pluck(1) |> 
-      filter(date == date_right | is.na(date)) |> 
-      filter(scale == df) |> 
-      pull(var)
+    break_labs_x <- variables$breaks_q3[
+      variables$var_code == unique(sub("_\\d{4}$", "", var_right))]
+    if (length(break_labs_x) > 0) break_labs_x <- 
+      break_labs_x[[1]]$var[
+        (break_labs_x[[1]]$date == date_left | 
+           is.na(break_labs_x[[1]]$date)) &
+          break_labs_x[[1]]$scale == df]
     
     # Format breaks
-    break_labels_y <- convert_unit(break_labels_y, var_left, TRUE)
-    break_labels_x <- convert_unit(break_labels_x, var_right, TRUE)
+    break_labs_y <- convert_unit(break_labs_y, var_left, TRUE)
+    break_labs_x <- convert_unit(break_labs_x, var_right, TRUE)
     
     # Construct result
-    break_labels <- list(x = break_labels_x, y = break_labels_y)
-    attr(break_labels, "qual") <- FALSE
+    break_labs <- list(x = break_labs_x, y = break_labs_y)
+    attr(break_labs, "qual") <- FALSE
      
   }
   
   
   ## Delta version -------------------------------------------------------------
   
-  if (data_type == "delta") {
-    
-    # Get break labels
-    break_labels <- 
-      variables |> 
-      filter(var_code == unique(sub("_\\d{4}$", "", var_left))) |> 
-      pull(breaks_q5) |> 
-      pluck(1) |> 
-      filter(scale == df)
-    
-    if (suppressWarnings(!is.null(break_labels$var_name) && 
-                         !any(is.na(break_labels$var_name)))) {
-      
-      qual <- TRUE
-      
-      break_labels <- 
-        break_labels |> 
-        filter(rank >= 1) |> 
-        pull(var_name_short)
-      
-    } else {
-      
-      qual <- FALSE
-      
-      break_labels <- break_labels$var
-      
-      # Format break labels
-      if (str_detect(var_left[1], "_pct|_dollar")) {
-        break_labels <- convert_unit(break_labels, var_left[1], TRUE)
-      }
-    }
-    
-    # Add qual attribute
-    attr(break_labels, "qual") <- qual
-    
-  }
+  # if (data_type == "delta") {
+  #   
+  #   # Get break labels
+  #   break_labs <- 
+  #     variables |> 
+  #     filter(var_code == unique(sub("_\\d{4}$", "", var_left))) |> 
+  #     pull(breaks_q5) |> 
+  #     pluck(1) |> 
+  #     filter(scale == df)
+  #   
+  #   if (suppressWarnings(!is.null(break_labs$var_name) && 
+  #                        !any(is.na(break_labs$var_name)))) {
+  #     
+  #     qual <- TRUE
+  #     
+  #     break_labs <- 
+  #       break_labs |> 
+  #       filter(rank >= 1) |> 
+  #       pull(var_name_short)
+  #     
+  #   } else {
+  #     
+  #     qual <- FALSE
+  #     
+  #     break_labs <- break_labs$var
+  #     
+  #     # Format break labels
+  #     if (str_detect(var_left[1], "_pct|_dollar")) {
+  #       break_labs <- convert_unit(break_labs, var_left[1], TRUE)
+  #     }
+  #   }
+  #   
+  #   # Add qual attribute
+  #   attr(break_labs, "qual") <- qual
+  #   
+  # }
   
   
   ## Delta bivariate version ---------------------------------------------------
   
-  if (data_type == "delta_bivar") {
-    
-    # Need to build breaks manually
-    break_labels_y <- 
-      data |> 
-      st_drop_geometry() |> 
-      summarize(
-        ranks = c(min(var_left, na.rm = TRUE),
-                  min(var_left[var_left_q3 == 2], na.rm = TRUE),
-                  min(var_left[var_left_q3 == 3], na.rm = TRUE),
-                  max(var_left, na.rm = TRUE))) |>
-      mutate(ranks = if_else(is.infinite(ranks), NA_real_, ranks)) |> 
-      pull(ranks) |> 
-      convert_unit("_pct", TRUE)
-
-    break_labels_x <- 
-      data |> 
-      st_drop_geometry() |> 
-      summarize(
-        ranks = c(min(var_right, na.rm = TRUE),
-                  min(var_right[var_right_q3 == 2], na.rm = TRUE),
-                  min(var_right[var_right_q3 == 3], na.rm = TRUE),
-                  max(var_right, na.rm = TRUE))) |>
-      mutate(ranks = if_else(is.infinite(ranks), NA_real_, ranks)) |> 
-      pull(ranks) |> 
-      convert_unit("_pct", TRUE)
-    
-    # Construct result
-    break_labels <- list(x = break_labels_x, y = break_labels_y)
-    attr(break_labels, "qual") <- FALSE
-    
-  }
+  # if (data_type == "delta_bivar") {
+  #   
+  #   # Need to build breaks manually
+  #   break_labs_y <- 
+  #     data |> 
+  #     st_drop_geometry() |> 
+  #     summarize(
+  #       ranks = c(min(var_left, na.rm = TRUE),
+  #                 min(var_left[var_left_q3 == 2], na.rm = TRUE),
+  #                 min(var_left[var_left_q3 == 3], na.rm = TRUE),
+  #                 max(var_left, na.rm = TRUE))) |>
+  #     mutate(ranks = if_else(is.infinite(ranks), NA_real_, ranks)) |> 
+  #     pull(ranks) |> 
+  #     convert_unit("_pct", TRUE)
+  # 
+  #   break_labs_x <- 
+  #     data |> 
+  #     st_drop_geometry() |> 
+  #     summarize(
+  #       ranks = c(min(var_right, na.rm = TRUE),
+  #                 min(var_right[var_right_q3 == 2], na.rm = TRUE),
+  #                 min(var_right[var_right_q3 == 3], na.rm = TRUE),
+  #                 max(var_right, na.rm = TRUE))) |>
+  #     mutate(ranks = if_else(is.infinite(ranks), NA_real_, ranks)) |> 
+  #     pull(ranks) |> 
+  #     convert_unit("_pct", TRUE)
+  #   
+  #   # Construct result
+  #   break_labs <- list(x = break_labs_x, y = break_labs_y)
+  #   attr(break_labs, "qual") <- FALSE
+  #   
+  # }
     
   ## Return output -------------------------------------------------------------
   
-  return(break_labels)
+  return(break_labs)
   
 }
