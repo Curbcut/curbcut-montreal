db49c2c6a046495afe09c75092b21d3da73a8693
Maxime Bélanger De Blois <maxime.belangerdeblois@mcgill.ca>
2022-03-24 15:34:37 -0400

    make dropdown updated

--- R/functions/__make_dropdown.R
+++ R/functions/__make_dropdown.R
@@ -1,37 +1,61 @@
-# Function to make the dropdown lists
+#### MAKE DROPDOWN LIST ########################################################
+#' @param multi_year A boolean. TRUE if the function should exclude variables
+#' that are not available at every census year. The function looks for the 
+#' maximum number of years available in all the wanted variables (after `only` 
+#' and `exclude`), and only keep the variables having this corresponding 
+#' number of years available.
+#' @param only A named list taking multiple vectors. Only the elements 
+#' corresponding are part of the dropdown. The list can take any column of the 
+#' `variables` table, but should probably be used with 3 levels: `theme`, 
+#' `source` and `var_code`.
+#' @param exclude A named list taking multiple vectors. All of its element will
+#' be excluded from the dropdown. The list can take any column of `variables` 
+#' table, but should probably be used with 3 levels: `theme`, `source` and
+#' `var_code`. The `exclude` code chunk runs AFTER `only`.
+#' @return A named list that can be supplied to `select_var_UI` and 
+#' `select_var_server`.
 
 make_dropdown <- 
-  function(exclude = NULL, multi_year = FALSE, include_only = NULL) {
-    
-    census_var <- variables[(variables$source == "census" &
-                              !is.na(variables$theme) &
-                              variables$theme != "Employment") |
-                              variables$theme %in% include_only, ]
+  function(multi_year = FALSE, only = list(source = "census"), 
+           exclude = NULL) {
 
-    if (!is.null(exclude)) {
-      census_var <- census_var[!census_var$theme %in% exclude, ]
+    vars <- variables
+    
+    if (!is.null(only)) {
+      if (!all(names(only) %in% names(vars))) {
+        stop(paste("A name of the `only` list doesn't correspond to a column ",
+                   "of the `variables` table."))
+      }
+      vars <- 
+        Reduce(\(df1, df2) {df1[df1$var_code %in% df2$var_code, ]}, 
+               lapply(names(only), \(x) vars[vars[[x]] %in% only[[x]], ]))
     }
     
-    if (multi_year) {
-      census_var <- census_var[lengths(census_var$dates) == 
-                                 max(lengths(census_var$dates))]
+    if (!is.null(exclude)) {
+      if (!all(names(exclude) %in% names(vars))) {
+        stop(paste("A name of the `exclude` list doesn't correspond to a ", 
+                   "column of the `variables` table."))
+      }
+      vars <- 
+        Reduce(\(df1, df2) {df1[df1$var_code %in% df2$var_code, ]}, 
+               lapply(names(exclude), \(x) vars[!vars[[x]] %in% exclude[[x]], ]))
     }
+    
+    if (multi_year) 
+      vars <- vars[lengths(vars$dates) == max(lengths(vars$dates)), ]
     
     out <- c("----" = " ",
-             lapply(setNames(unique(census_var$theme),
-                             unique(census_var$theme)), \(cat) {
-               cat_vecs <- 
-                 census_var[census_var$theme == cat, c("var_code", "var_title")]
-               
-               lapply(cat_vecs$var_title, \(name) {
-                 cat_vecs[cat_vecs$var_title == name, ]$var_code}) |> 
-                 setNames(cat_vecs$var_title)
-             })
+             lapply(setNames(unique(vars$theme),
+                             unique(vars$theme)), \(cat) {
+                               cat_vecs <- 
+                                 vars[vars$theme == cat, c("var_code", "var_title")]
+                               
+                               lapply(cat_vecs$var_title, \(name) {
+                                 cat_vecs[cat_vecs$var_title == name, ]$var_code}) |> 
+                                 setNames(cat_vecs$var_title)
+                             })
     )
-    
-    if (!is.null(include_only)) {
-      out <- out[names(out) %in% include_only]
-    }
     
     return(out)
   }
+
