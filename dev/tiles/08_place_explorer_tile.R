f7afc82c3887760a0fc3e835c2d805f397ad7518
Maxime Bélanger De Blois <maxime.belangerdeblois@mcgill.ca>
2022-03-29 11:56:44 -0400

    updated place explorer tiles

--- dev/tiles/08_place_explorer_tile.R
+++ dev/tiles/08_place_explorer_tile.R
@@ -11,28 +11,20 @@ source("dev/tiles/tile_functions.R")
 DA |> 
   select(DAUID, CTUID, CSDUID) |> 
   st_set_agr("constant") |> 
-  upload_tile_source("place_explorer-DA2", "maxbdb2", .sus_token)
+  upload_tile_source("place_explorer-DA", .sus_token)
 
 
 # Add recipes -------------------------------------------------------------
 
-recipe_DA <- '
-{
-  "recipe": {
-    "version": 1,
-    "layers": {
-      "DA": {
-        "source": "mapbox://tileset-source/maxbdb2/place_explorer-DA2",
-        "minzoom": 8,
-        "maxzoom": 13
-      }
-    }
-  },
-  "name": "place_explorer-DA2"
-}
-'
+recipe_DA <- 
+  create_recipe(
+    layer_names = "DA",
+    source = "mapbox://tileset-source/sus-mcgill/place_explorer-DA",
+    minzoom = 8,
+    maxzoom = 13, 
+    recipe_name = "place_explorer-DA")
 
 # Create and publish tilesets ---------------------------------------------
 
-create_tileset("place_explorer-DA2", recipe_DA, "maxbdb2")
-publish_tileset("place_explorer-DA2", "maxbdb2")
+create_tileset("place_explorer-DA", recipe_DA)
+publish_tileset("place_explorer-DA")
