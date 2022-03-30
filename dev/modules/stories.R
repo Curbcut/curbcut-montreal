4897a546bdf6770ed383b8e265ab8eb99d676e53
Maxime Bélanger De Blois <maxime.belangerdeblois@mcgill.ca>
2022-03-30 19:41:04 -0400

    new stories push

--- dev/modules/stories.R
+++ dev/modules/stories.R
@@ -29,29 +29,30 @@ stories <-
     lon = -73.574962, 
     lat = 45.479311) |> 
   
-  # add_row(
-  #   name = "old_quarry",
-  #   title = "The old quarry",
-  #   img = "old_quarry.png",
-  #   preview = paste0("Quarry test"),
-  #   lon = -73.61389,
-  #   lat = 45.57222) |> 
+  add_row(
+    name = "mirron_quarry",
+    title = "The Miron Quarry: The Transformation of an Industrial Limestone Quarry into a Flourishing Environmental Complex",
+    img = "mirron_quarry.png",
+    preview = "Mirron Quarry",
+    lon = -73.61389,
+    lat = 45.57222) |>
   
   add_row(
     name = "parc_ouest",
     title = "The Grand parc de l'ouest",
     img = "parc_ouest.png",
-    preview = paste0("Park test"),
+    preview = "The Grand parc de l'ouest",
     lon = -73.93723394534209,
     lat = 45.45384833740566) |> 
   
-  # add_row(
-  #   name = "flooding_pierrefonds",
-  #   title = "Flooding in Pierrefonds",
-  #   img = "flooding_pierrefonds.png",
-  #   preview = paste0("Flooding test"),
-  #   lon = -73.85489584811597,
-  #   lat = 45.49182349238028) |> 
+  add_row(
+    name = "turcot_interchange",
+    title = paste0("The Reconstruction of the Turcot Interchange: The ",
+                   "Intersection of Community Desires and Government Priorities"),
+    img = "flooding_pierrefonds.png",
+    preview = paste0("Turcot interchange"),
+    lon = -73.600007,
+    lat = 45.468355) |>
   
   add_row(
     name = "griffintown",
