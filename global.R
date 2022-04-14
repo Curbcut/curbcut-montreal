5e5ca40c5fc7b8c47fa9a8419c474d0404c9ac6c
Maxime Bélanger De Blois <maxime.belangerdeblois@mcgill.ca>
2022-04-14 15:55:23 -0400

    loading natural infrastructure qsm

--- global.R
+++ global.R
@@ -45,7 +45,7 @@ qload("data/alley.qsm")
 # crash <- qread("data/crash.qs")
 tt_matrix <- qread("data/tt_matrix.qs")
 # marketed_sustainability <- qread("data/marketed_sustainability.qs")
-# qload("data/natural_infrastructure.qsm")
+qload("data/natural_infrastructure.qsm")
 metro_lines <- qread("data/metro_lines.qs")
 
 stories <- qread("data/stories.qs")
