4479ceeb29f3c7740ae3a98e4b3db92a2b8223f7
dwachsmuth <david.wachsmuth@mcgill.ca>
2022-04-21 12:34:17 -0400

    deploy_sus

--- dev/other/deploy_sus.R
+++ dev/other/deploy_sus.R
@@ -4,35 +4,44 @@ deploy_sus <- function(app_name = "sus-mssi", ...) {
   
   # Temporarily update server.R
   server_to_update <- readLines("server.R")
-  which_line_s <- stringr::str_which(server_to_update, 'updateQueryString\\("\\?"\\)')
+  which_line_s <- stringr::str_which(server_to_update, 
+                                     'updateQueryString\\("\\?"\\)')
   server_to_update[which_line_s] <- r"(    # updateQueryString("?"))"
   writeLines(server_to_update, "server.R")
   
-  # Temporarily update m_bookmark.R
-  bookmark_to_update <- readLines("R/m_bookmark.R")
-  which_line_b <- stringr::str_which(bookmark_to_update, 'updateQueryString\\(url\\)')
-  if (app_name == "sus-mssi") {
-    bookmark_to_update[which_line_b] <- 
-      r"(      updateQueryString(paste0("/sus-mssi", url)))"   
-  } else if (app_name == "sus-dev") {
-    bookmark_to_update[which_line_b] <- 
-      r"(      updateQueryString(paste0("/sus-dev", url)))" 
-  } else if (app_name == "sus-dev2") {
-    bookmark_to_update[which_line_b] <- 
-      r"(      updateQueryString(paste0("/sus-dev2", url)))" 
-  } else if (app_name == "sus-dev3") {
-    bookmark_to_update[which_line_b] <- 
-      r"(      updateQueryString(paste0("/sus-dev3", url)))" 
-  }
-  writeLines(bookmark_to_update, "R/m_bookmark.R")
-  
-  # Set on.exit to restore server.R and m_bookmark.R
+  # Set on.exit to restore server.R
   on.exit({
     server_to_update[which_line_s] <- r"(    updateQueryString("?"))"
     writeLines(server_to_update, "server.R")
-    bookmark_to_update[which_line_b] <- r"(      updateQueryString(url))"
-    writeLines(bookmark_to_update, "R/m_bookmark.R")  
   })
+  
+  if (app_name != "sus-mssi") { 
+    
+    # Temporarily update m_bookmark.R
+    bookmark_to_update <- readLines("R/m_bookmark.R")
+    which_line_b <- stringr::str_which(bookmark_to_update, 
+                                       'updateQueryString\\(url\\)')
+    if (app_name == "sus-dev") {
+      bookmark_to_update[which_line_b] <- 
+        r"(      updateQueryString(paste0("/sus-dev", url)))" 
+    } else if (app_name == "sus-dev2") {
+      bookmark_to_update[which_line_b] <- 
+        r"(      updateQueryString(paste0("/sus-dev2", url)))" 
+    } else if (app_name == "sus-dev3") {
+      bookmark_to_update[which_line_b] <- 
+        r"(      updateQueryString(paste0("/sus-dev3", url)))" 
+    }
+    writeLines(bookmark_to_update, "R/m_bookmark.R")
+    
+    # Set on.exit to restore server.R and m_bookmark.R
+    on.exit({
+      server_to_update[which_line_s] <- r"(    updateQueryString("?"))"
+      writeLines(server_to_update, "server.R")
+      bookmark_to_update[which_line_b] <- r"(      updateQueryString(url))"
+      writeLines(bookmark_to_update, "R/m_bookmark.R")  
+    })
+  }
+  
   
   # Deploy app
   rsconnect::deployApp(appName = app_name, forceUpdate = TRUE, ...)
