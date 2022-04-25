0ba5db4bd02cbf9a63194b6f634f6fcaae6d2465
Maxime Bélanger De Blois <maxime.belangerdeblois@mcgill.ca>
2022-04-25 18:52:56 -0400

    grid to standard df()

--- R/functions/_get_var_type.R
+++ R/functions/_get_var_type.R
@@ -5,7 +5,7 @@ get_var_type <- function(data, var_left, var_right, df, select_id,
   
   ## Invalidate if non-standard df() -------------------------------------------
   
-  if (!df %in% c("borough", "CT", "DA", "building", 
+  if (!df %in% c("borough", "CT", "DA", "building", "grid",
                  "street", "heatmap", "point")) return(NULL)
   
   
