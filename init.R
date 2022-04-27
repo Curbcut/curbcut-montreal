250d7245e4af507f96a2dea2c22b9f73b4b87dda
dwachsmuth <david.wachsmuth@mcgill.ca>
2022-04-27 12:21:24 -0400

    Heroku testing

---
+++ init.R
@@ -0,0 +1,6 @@
+#### INITIALIZE PACKAGES #######################################################
+
+helpers.installPackages("remotes", "shiny", "shinyjs", "shinyWidgets",
+                        "ggplot2", "stringr", "glue", "qs", "metathis")
+
+remotes::install_github("anthonynorth/rdeck")
