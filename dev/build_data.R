3d2543a73eef635bb07ec3a9532152d5626ecbee
Maxime Bélanger De Blois <maxime.belangerdeblois@mcgill.ca>
2022-07-06 15:44:45 -0400

    tweak to build data

--- dev/build_data.R
+++ dev/build_data.R
@@ -277,9 +277,7 @@ library(stringr)
 qload("data2/natural_inf.qsm")
 tt_matrix <- qread("data2/tt_matrix.qs")
 building <- qread("data2/building.qs")
-grid <- qread("data2/grid.qs") |> 
-  # Until the data gets redrawn!
-  dplyr::select(ID:households, ends_with("_2016"), starts_with("climate"))
+grid <- qread("data2/grid.qs")
 
 sqlite_path <- "data/sql_db.sqlite"
 
