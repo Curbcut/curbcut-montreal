c43469b06a2907aa8e2b919f2602091380b55935
Maxime Bélanger De Blois <maxime.belangerdeblois@mcgill.ca>
2022-09-19 10:26:04 -0400

    buildings updated, using OSM instead of expensive google API

--- dev/geometries/building_geocode.R
+++ dev/geometries/building_geocode.R
@@ -1,39 +1,82 @@
 #### Reverse geocode building centroids ########################################
 # Dependent script: needs 'building' object
 
-# This script makes thousands of expensive calls to Google's geocoding API, so
+# This script makes thousands of calls to OSM geocoding API, so
 # the geocoding portion should only be run a single time!
+
+# Bind address if found previously ----------------------------------------
+
+old_building <- 
+  qread("data2/building_full.qs") |> 
+  transmute(old_ID = as.numeric(ID), name) |>
+  arrange(old_ID) |> 
+  st_transform(32618)
+
+new_centroids <-
+  building |>
+  st_transform(32618) |>
+  st_centroid()
+
+same_building <- st_intersects(new_centroids, old_building)
+
+building$old_ID <- 
+  map_int(same_building, ~{if (length(.x[1]) == 0) NA else .x[1]})
+
+building <- 
+  building |> 
+  left_join(st_drop_geometry(old_building), by = "old_ID") |> 
+  relocate(name, .before = name_2) |> 
+  select(-old_ID)
+
 
 # Geocode with frequent saves ---------------------------------------------
 
-# library(ggmap)
-# 
-# building <- qread("dev/data/building.qs", nthreads = future::availableCores())
-# 
-# address <-
-#   building %>%
-#   transmute(ID, name = NA_character_,)
-# 
-# centroids <- 
-#   building |> 
-#   st_transform(32618) |> 
-#   st_centroid() |> 
-#   st_transform(4326) |> 
-#   pull(geometry)
-# 
-# for (i in seq_along(building$ID)) {
-# 
-#   print(i)
-#   if (is.na(address[i,]$name)) address[i,]$name <- revgeocode(centroids[[i]])
-#   if (i %% 1000 == 0) qsave(address, "dev/data/building_geocode.qs",
-#                             nthreads = future::availableCores())
-# 
-# }
-# 
-# qsave(address, "dev/data/building_geocode.qs", 
-#       nthreads = future::availableCores())
-# rm(i, centroids)
+address <-
+  building |> 
+  select(ID, name)
 
+centroids <-
+  address |>
+  st_transform(32618) |>
+  st_centroid() |> 
+  st_transform(4326) |>
+  mutate(lat = st_coordinates(geometry)[,"X"],
+         lon = st_coordinates(geometry)[,"Y"])
+
+reverse_geocode <- function(lat, lon) {
+  
+  paste0("https://nominatim.openstreetmap.org/reverse?format=json&lat=", 
+         centroids$lon[i], "&lon=", centroids$lat[i], "&addressdetails=1") |> 
+    httr::GET() |> 
+    httr::content() |> 
+    pluck("address")
+  
+}
+
+for (i in seq_along(centroids$ID)) {
+  if (is.na(address[i,]$name)) {
+    start <- Sys.time()
+    
+    z <- 
+      paste0("https://nominatim.openstreetmap.org/reverse?format=xml&lat=", 
+             centroids$lon[i], "&lon=", centroids$lat[i], "&addressdetails=1") |> 
+      httr::GET() |> 
+      httr::content() |> 
+      rvest::html_element("result") |> 
+      rvest::html_text()
+    
+    message('\r', paste0(i, "/", nrow(centroids), "  Last found: ", z), 
+            appendLF = FALSE)
+    
+    address$name[i] <- z
+    
+    end <- Sys.time()
+    # Respect the 1 request per second
+    if (as.numeric(end - start) < 1) Sys.sleep(1 - as.numeric(end - start))
+  }
+  if (i %% 1000 == 0) qsave(address, "dev/data/building_geocode.qs",
+                            nthreads = future::availableCores())
+}
 
 address <- qread("dev/data/building_geocode.qs",
                  nthreads = future::availableCores())
@@ -43,9 +86,7 @@ address <- qread("dev/data/building_geocode.qs",
 
 names <-
   address |> 
-  mutate(name = str_remove(name, ", QC.*$")) |>
-  mutate(name = if_else(str_detect(name, ", .*,"), str_remove(name, "^[^,]*, "),
-                        name)) |> 
+  mutate(name = str_remove(name, ", [^,]*, [^,]*, [^,]*, [^,]*$")) |> 
   pull(name)
 
 
