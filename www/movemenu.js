70542dd38210e4d0615775dcafeeead3bad4a055
Giovanni De Franceschi <dott.defranceschi@gmail.com>
2023-03-26 19:26:37 -0400

    - removed unused js

--- www/movemenu.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener('load', function() {
-  var dropdownMenu = document.querySelector('.dropdown-menu');
-  var xPosition = 0;
-
-  document.addEventListener('mousemove', function(event) {
-    xPosition = event.clientX;
-    dropdownMenu.style.left = xPosition + 'px';
-  });
-});
--- www/test.js
+++
@@ -1,5 +0,0 @@
-const elements = document.querySelectorAll('.optgroup-8');
-
-elements.forEach((element) => {
-  element.style.backgroundColor = 'red'; // replace 'red' with the desired color
-});
