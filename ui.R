0e5894a9d6d798ead7504307d3e318f526176dd2
Maxime Bélanger De Blois <maxime.belangerdeblois@mcgill.ca>
2023-08-01 10:50:43 -0400

    Update share tags

--- ui.R
+++ ui.R
@@ -1,4 +1,5 @@
 curbcut::ui(
+  site_name = site_name,
   web_description = paste0(
     "Curbcut est une plateforme d'exploration approfondie, dynamique et ",
     "intuitive de la durabilité urbaine."
@@ -6,5 +7,6 @@ curbcut::ui(
   web_title = paste0(site_name, " | Vers une ville durable"), 
   twitter_handler = "@curbcutca", 
   google_analytics = "www/google_analytics.html", 
-  website_url = "https://montreal.curbcut.ca", 
-  share_jpg = "https://montreal.curbcut.ca/share.jpg")
+  website_url = "https://beta.curbcut.ca", 
+  share_jpg = "https://beta.curbcut.ca/share.jpg",
+  apple_touch_icon = "https://beta.curbcut.ca/logo192.jpg")
